3d40a5fd61bfc299b415a9fb75c993188f2cba6f
BorgesDev <borgesarthur.dev@gmail.com>
2024-02-20 23:16:56 -0300

    0.4 Alert with textView test

--- Tento/Game/View/GameView.swift
+++ Tento/Game/View/GameView.swift
@@ -1,53 +1,54 @@
 import SwiftUI
-struct PlayerNameEditView: View {
-    @Binding var playerName: String
-    @State private var editedPlayerName: String
-    @Binding var isPresented: Bool
-
-    init(playerName: Binding<String>, isPresented: Binding<Bool>) {
-        self._playerName = playerName
-        self._editedPlayerName = State(initialValue: playerName.wrappedValue)
-        self._isPresented = isPresented
-    }
-
-    var body: some View {
-        VStack {
-            Text("Novo nome :")
-                .font(.title.bold())
-                .padding(.bottom, 10)
-            TextField("Nos", text: $editedPlayerName)
-                .padding(.vertical, 6)
-                .multilineTextAlignment(.center)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10.0)
-                        .stroke(lineWidth: 0.8)
-                )
-                .frame(width: 100)
-                .disableAutocorrection(true)
-                .autocapitalization(.none)
-//                .padding(.horizontal,100)
-
-            Button("Salvar") {
-                playerName = editedPlayerName
-                isPresented = false
-            }
-            .frame(width: 70)
-            .padding(.vertical, 4)
-            .padding(.horizontal, 5)
-            .font(.callout)
-            .background(Color(.blue))
-            .foregroundColor(.white)
-            .cornerRadius(10.0)
-            .padding(.top, 15)
-        }
-        .padding()
-    }
-}
+//struct PlayerNameEditView: View {
+//    @Binding var playerName: String
+//    @State private var editedPlayerName: String
+//    @Binding var isPresented: Bool
+//
+//    init(playerName: Binding<String>, isPresented: Binding<Bool>) {
+//        self._playerName = playerName
+//        self._editedPlayerName = State(initialValue: playerName.wrappedValue)
+//        self._isPresented = isPresented
+//    }
+//
+//    var body: some View {
+//        VStack {
+//            Text("Novo nome :")
+//                .font(.title.bold())
+//                .padding(.bottom, 10)
+//            TextField("Nos", text: $editedPlayerName)
+//                .padding(.vertical, 6)
+//                .multilineTextAlignment(.center)
+//                .overlay(
+//                    RoundedRectangle(cornerRadius: 10.0)
+//                        .stroke(lineWidth: 0.8)
+//                )
+//                .frame(width: 100)
+//                .disableAutocorrection(true)
+//                .autocapitalization(.none)
+////                .padding(.horizontal,100)
+//
+//            Button("Salvar") {
+//                playerName = editedPlayerName
+//                isPresented = false
+//            }
+//            .frame(width: 70)
+//            .padding(.vertical, 4)
+//            .padding(.horizontal, 5)
+//            .font(.callout)
+//            .background(Color(.blue))
+//            .foregroundColor(.white)
+//            .cornerRadius(10.0)
+//            .padding(.top, 15)
+//        }
+//        .padding()
+//    }
+//}
 struct GameView: View {
     
     @ObservedObject var viewModel: GameViewModel
     @State private var isEditingPlayer1Name = false
     @State private var isEditingPlayer2Name = false
+    @State private var newUsername = ""
     
     var body: some View {
         ZStack {
@@ -58,9 +59,17 @@ struct GameView: View {
                         .onTapGesture {
                             isEditingPlayer1Name.toggle()
                         }
-                        .sheet(isPresented: $isEditingPlayer1Name){
-                            PlayerNameEditView(playerName: $viewModel.player1.name, isPresented: $isEditingPlayer1Name)
+                        .alert("Mudar Nome", isPresented: $isEditingPlayer1Name) {
+                                TextField("Novo nome", text: $newUsername)
+                                   Button ("Salvar"){
+                                           guard !newUsername.isEmpty else {
+                                               return
+                                           }
+                                             viewModel.player1.name = newUsername
+                                       }
+                                   Button("Cancel", role: .cancel, action: {})
                         }
+
 
                     Text("\(viewModel.player1.score)")
                         .font(.largeTitle)
@@ -88,9 +97,9 @@ struct GameView: View {
                             .onTapGesture {
                                 isEditingPlayer2Name.toggle()
                             }
-                            .sheet(isPresented: $isEditingPlayer2Name){
-                                PlayerNameEditView(playerName: $viewModel.player2.name, isPresented: $isEditingPlayer2Name)
-                            }
+//                            .sheet(isPresented: $isEditingPlayer2Name){
+//                                PlayerNameEditView(playerName: $viewModel.player2.name, isPresented: $isEditingPlayer2Name)
+//                            }
                         Text("\(viewModel.player2.score)")
                                   .font(.largeTitle)
                                 
